9dee26fb66700f68e0ad14cf77f4996f6a27986a
Mukund70 <mukundbansal10@gmail.com>
2024-11-10 20:18:13 +0530

    updated

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,11 +4,13 @@ import {ReactNode} from 'react';
 
 type PropsType = {
     children: ReactNode;
-}
+    onClick?:() => void;
+};
 
-const Button = ({children}:PropsType) =>{
-  return( <button style={{
-
+const Button = ({children,onClick}:PropsType) =>{
+  return( <button 
+    onClick={onClick}
+    style={{
     backgroundColor: 'blue',
     color: 'white',
     padding: '10px 20px',
